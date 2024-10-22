f8f4d4a294bd324855ba79a4e63af87532a6314c
hagiumarius <marius.hagiu@gmail.com>
2024-10-22 11:59:07 +0300

    tests

--- src/test/java/com/unitedInternet/WebApplicationIntegrationTest.java
+++ src/test/java/com/unitedInternet/WebApplicationIntegrationTest.java
@@ -1,48 +1,61 @@
 package com.unitedInternet;
 
 import com.unitedinternet.filestore.FilestoreApplication;
-import com.unitedinternet.filestore.service.FileStorageResolver;
-import org.hamcrest.Matchers;
+import jakarta.servlet.ServletContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @ExtendWith(SpringExtension.class)
-@AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
-        classes = FilestoreApplication.class)
+@ContextConfiguration(classes = {FilestoreApplication.class })
+@WebAppConfiguration
 @TestPropertySource(
         locations = "classpath:application-integrationtest.properties")
-public class FileStorageControllerIntegrationTest {
+public class WebApplicationIntegrationTest {
 
     @Autowired
+    private WebApplicationContext webApplicationContext;
+
     private MockMvc mockMvc;
 
-    @Mock
-    FileStorageResolver fileStorageResolver;
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    public void givenWac_whenServletContext_thenItProvidesMemberController() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        assertNotNull(webApplicationContext.getBean("fileStorageController"));
+        assertNotNull(webApplicationContext.getBean("fileStorageAdminController"));
+    }
 
     @Test
     public void givenTEST_whenGet_thenReturnsOk() throws Exception {
-        MvcResult mvcResult = this.mockMvc.perform(get("/files/test")
-                        .contentType("application/json")
-                        .header("Authorization","Bearer sample"))
+        MvcResult mvcResult = this.mockMvc.perform(get("/files/test").contentType("application/json"))
                 .andDo(print()).andExpect(status().isOk())
                 .andReturn();
     }
@@ -52,13 +65,8 @@ public class FileStorageControllerIntegrationTest {
         MockMultipartFile contentFile = new MockMultipartFile("file", "test.txt", MediaType.TEXT_PLAIN_VALUE, "Whatever Content".getBytes());
         mockMvc.perform(multipart("/files")
                         .file(contentFile)
-                        .param("path","/categories/binoculars/hunting")
-                        .header("Authorization","Bearer sample"))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.statusCode").value("201"))
-                .andExpect(jsonPath("$.message").value(containsString("File uploaded successfully")))
-                .andExpect(jsonPath("$.details.id").value(containsString("test.txt")));
-
+                        .param("path","/categories/binoculars/hunting"))
+                .andExpect(status().isCreated());
 
     }
 
